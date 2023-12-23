8d0a6a6b9071166889982cac1da058fff88213f5
fadamovic <filip.adamovic@syrmia.com>
2023-12-23 14:02:50 +0100

    #48 #33 #34 tests update

--- tests/tests_functions.py
+++ tests/tests_functions.py
@@ -1,8 +1,9 @@
 import unittest
+from unittest.mock import patch
 import hash_module
 import sys
 sys.path.append("..")
-from functions import User
+from functions import *
 
 class TestCalculatorModule(unittest.TestCase):
 
@@ -10,17 +11,34 @@ class TestCalculatorModule(unittest.TestCase):
         pass
 
     def tearDown(self):
-        pass
+        logged_in_users.clear()
+        registered_users.clear()
 
-    def test_register_valid_username_password(self):
+    def test_register_valid_username_password(self,):
             valid_password = "1234567890"
             user = User("test_user", valid_password)
             self.assertEqual(user.password, valid_password)
 
-
-    def test_register_invalid_password_length(self):
+    
+    def test_register_invalid_password_length(self,):
         with self.assertRaises(ValueError):
             user = User("testUser","1234567891234")
+        
+
+    @patch("functions.User._validate_password")
+    def test_register_invalid_password_length_pass_with_mock(self,mock_validate):
+        username="testUser"
+        invalid_password="1234567891234"
+        mock_validate.return_value=invalid_password
+        user = User(username,invalid_password)
+        self.assertEqual(user.username,username)
+        self.assertEqual(user.password,invalid_password)
+
+    @patch("functions.User.__len__")
+    def test_length_of_contact_list(self,mock_len):
+        mock_len.return_value=100
+        user=User("a","a")
+        self.assertEqual(len(user),100)
             
     def test_username_setter(self):
         username="Damjan"
@@ -44,6 +62,30 @@ class TestCalculatorModule(unittest.TestCase):
         user=User("Vedran",password)
         self.assertEqual(user.password,password)
 
+    def test_login_with_valid_parameters(self):
+        username="Vedran Jovanovic"
+        password="1234567890"
+        register(username, password)
+        login(username, password)
+        self.assertTrue(username in logged_in_users)
+
+    def test_login_with_invalid_username(self):
+        print(registered_users) 
+        username="Vedran Jovanovic"
+        password="1234567890"
+        register(username, password)
+        username="Vedran"
+        login(username, password)
+        self.assertTrue(username not in logged_in_users)
+        
+    def test_login_with_invalid_password(self):
+        print(registered_users) 
+        username="Vedran Jovanovic"
+        password="1234567890"
+        register(username, password)
+        password="987654321"
+        login(username, password)
+        self.assertTrue(username not in logged_in_users)
 
 
 if __name__ == '__main__':
